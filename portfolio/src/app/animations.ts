20ba859b2c16c335651f5e8e4447db0971216dab
irkaal <irkaalv@gmail.com>
2020-08-22 16:42:54 -0700

    Moved project source code to source branch

--- portfolio/src/app/animations.ts
+++
@@ -1,9 +0,0 @@
-import { trigger, style, animate, transition, query, group, keyframes } from '@angular/animations';
-
-export const slideInAnimation =
-    trigger('routeAnimations', [
-        transition('* <=> *', [
-            style({ opacity: 0 }),
-            animate('1.5s ease', style({ opacity: 1 }))
-        ])
-    ]);
