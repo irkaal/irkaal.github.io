2d7a62052cafcbabd18e0b50f3945a9c1fad3851
irkaal <irkaalv@gmail.com>
2020-08-22 16:34:57 -0700

    Minor refactoring

--- portfolio/src/app/blog/blog-post/blog-post.component.ts
+++ portfolio/src/app/blog/blog-post/blog-post.component.ts
@@ -1,5 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, AfterViewChecked } from '@angular/core';
-import {HighlightService} from '../../highlight.service';
+import { ActivatedRoute } from '@angular/router';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { combineLatest } from 'rxjs';
+import { map, pluck } from 'rxjs/operators';
+
+import { HighlightService } from '../../highlight.service';
 
 @Component({
   selector: 'app-blog-post',
@@ -7,13 +13,30 @@ import {HighlightService} from '../../highlight.service';
   styleUrls: ['./blog-post.component.scss']
 })
 export class BlogPostComponent implements OnInit, AfterViewChecked {
+  githubAvatar: string;
 
-  constructor(private highlightService: HighlightService) { }
+  constructor(
+    private highlightService: HighlightService,
+    private activatedRoute: ActivatedRoute,
+    private scully: ScullyRoutesService,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.http.get(
+      'https://api.github.com/users/irkaal'
+    ).subscribe((data: any) => {
+      this.githubAvatar = data['avatar_url'];
+    });
   }
 
   ngAfterViewChecked() {
     this.highlightService.highlightAll();
   }
+
+  $postMetadata = combineLatest([
+    this.activatedRoute.params.pipe(pluck('postId')), this.scully.available$
+  ]).pipe(
+    map(([postId, routes]) => routes.find(route => route.route === `/blog/${postId}`))
+  );
 }
